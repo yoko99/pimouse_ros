dc39f6ba5c80914547d23177aca66a4831428689
Takaaki Yokoyama <yoko99@hat.hi-ho.ne.jp>
2023-01-16 03:06:35 +0000

    Add package files

--- scripts/buzzer4.py
+++ scripts/buzzer4.py
@@ -15,25 +15,7 @@ def write_freq(hz=0):
 def recv_buzzer(data):
     write_freq(data.data)
 
-def exec_music(goal):
-    r = MusicResult()
-    fb = MusicFeedback()
-
-    for i, f in enumerate(goal.freqs):
-        fb.remaining_steps = len(goal.freqs) - i
-        music.publish_feedback(fb)
-
-        if music.is_preempt_requested():
-            write_freq(0)
-            r.finished = False
-            music.set_preempted(r)
-            return
-
-        write_freq(f)
-        rospy.sleep(1.0 if i >= len(goal.durations) else goal.durations[i])
-
-    r.finished = True
-    music.set_succeeded(r)
+def exec_music(goal): pass
 
 if __name__ == '__main__':
     rospy.init_node('buzzer')
---
+++ scripts/lightsensors.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import sys, rospy
+from pimouse_ros.msg import LightSensorValues
+
+def get_freq():
+    f = rospy.get_param('lightsensors_freq' ,10)
+    #print f
+    try:
+        if f <= 0.0:
+            raise Exception()
+    except:
+        rospy.logerr("value error: lightsensors_freq")
+        sys.exit(1)
+
+    return f
+
+if __name__ == '__main__':
+    devfile = '/dev/rtlightsensor0'
+    rospy.init_node('lightsensors')
+    pub = rospy.Publisher('lightsensors', LightSensorValues, queue_size=1)
+
+    freq = get_freq()
+    #print freq
+    rate = rospy.Rate(freq)
+    while not rospy.is_shutdown():
+        try:
+            with open(devfile, 'r') as f:
+                data = f.readline().split()
+                data = [ int(e) for e in data ]
+                d = LightSensorValues()
+                d.right_forward - data[0]
+                d.right_side = data[1]
+                d.left_side = data[2]
+                d.left_forward = data[3]
+                d.sum_all = sum(data)
+                d.sum_forward = data[0] + data[3]
+                pub.publish(d)
+        except IOError:
+            rospy.logerr("cannot write to " + devfile)
+
+        f = get_freq()
+        if f != freq:
+            freq = f
+            rate = raspy.Rate(freq)
+
+        rate.sleep()
