f2c5d133856a00f2d2ff8d8d03e700d00975f323
Takaaki Yokoyama <yoko99@hat.hi-ho.ne.jp>
2023-01-27 00:47:14 +0000

    yaml file was modified for chap7.2, travis_test_buzzer.py was modified

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -11,11 +11,11 @@ class BuzzerTest(unittest.TestCase):
 
     def test_put_value(self):
         pub = rospy.Publisher('/buzzer', UInt16)
-        for i in range(10)
+        for i in range(10):
             pub.publish(1234)
             time.sleep(0.1)
 
-        with open("/dev/rtbuzzer0","r") as f
+        with open("/dev/rtbuzzer0","r") as f:
             data = f.readline()
             self.assertEqual(data,"1234/n","value dose not written to rtbuzzer0")
 
