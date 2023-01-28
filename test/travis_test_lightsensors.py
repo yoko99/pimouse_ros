688d256f714f8389203971ffab1284ff3f45b44e
Takaaki Yokoyama <yoko99@hat.hi-ho.ne.jp>
2023-01-28 00:56:29 +0000

    travis_test_lightsensors.py was modified

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -39,7 +39,7 @@ class LightsensorTest(unittest.TestCase):
         self.check_values(4321,123,0,-1)
 
     def test_change_parameter(self):
-        rospy.set_param('lightsensours_freq', 1)
+        rospy.set_param('lightsensors_freq', 1)
         time.sleep(2)
         c_prev = self.count
         time.sleep(3)
