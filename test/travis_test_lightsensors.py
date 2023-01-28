94a9f1d97ab60ea38ae3c92967ca0666813af753
Takaaki Yokoyama <yoko99@hat.hi-ho.ne.jp>
2023-01-28 03:09:57 +0000

    travis_test_lightsensors.py was modified

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -21,7 +21,7 @@ class LightsensorTest(unittest.TestCase):
         self.assertEqual(vs.left_forward, lf, "different value: left_forward")
         self.assertEqual(vs.left_side, ls, "different value: left_side")
         self.assertEqual(vs.right_side, rs, "different value: right_side")
-        self.assertEqual(vs.right_forward, rf, "different value: right_forward")
+        self.assertEqual(vs.right_forward, lf, "different value: right_forward")
         self.assertEqual(vs.sum_all, lf+ls+rs+rf, "different value: sum_all")
         self.assertEqual(vs.sum_forward, lf+rf, "different value: sum_forward")
 
