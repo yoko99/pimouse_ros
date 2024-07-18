b3880ddd631ad0a7b63ae0175bd7944b515755f3
Takaaki Yokoyama <yoko99@hat.hi-ho.ne.jp>
2024-07-18 16:26:02 +0900

    travis_test_buzzer.py was modified

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -20,7 +20,7 @@ class BuzzerTest(unittest.TestCase):
         with open("/dev/rtbuzzer0","r") as f:
             data = f.readline()
             print(data)
-            self.assertEqual(data,"1234\n","value dose not written to rtbuzzer0")
+            self.assertEqual(data,"1234","value dose not written to rtbuzzer0")
 
 if __name__ == '__main__':
     time.sleep(3)
