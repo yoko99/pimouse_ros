952d7acbc064b0553642d68382723f2c11568fbe
Takaaki Yokoyama <yoko99@hat.hi-ho.ne.jp>
2024-07-18 14:55:31 +0900

    The check point which was written by python2 was modified.

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -3,11 +3,23 @@
 import rospy, unittest, rostest, actionlib
 import rosnode
 import time
+from std_msgs.msg import UInt16
+from pimouse_ros.msg import MusicAction, MusicResult, MusicFeedback, MusicGoal
 
 class BuzzerTest(unittest.TestCase):
     def test_node_exist(self):
         nodes = rosnode.get_node_names()
         self.assertIn('/buzzer' ,nodes, "node dose not exist")
+
+    def test_put_value(self):
+        pub = rospy.Publisher('/buzzer', UInt16)
+        for i in range(10):
+            pub.publish(1234)
+            time.sleep(0.1)
+
+        with open("/dev/rtbuzzer0","r") as f:
+            data = f.readline()
+            self.assertEqual(data,"1234\n","value dose not written to rtbuzzer0")
 
 if __name__ == '__main__':
     time.sleep(3)
