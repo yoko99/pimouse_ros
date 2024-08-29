470c8134342c76c76d6f8754ffe022d60d9a51a6
Takaaki Yokoyama <yoko99@hat.hi-ho.ne.jp>
2024-08-29 15:53:32 +0900

    travis_test_buzzer.py was created

--- test/travis_test_motors2.py
+++ test/travis_test_motors2.py
@@ -5,12 +5,14 @@ import rosnode, rospy
 import time
 from pimouse_ros.msg import MotorFreqs
 from geometry_msgs.msg import Twist
-from std_srvs.srv import Trigger, TriggerResponse  #追加
+from std_srvs.srv import Trigger, TriggerResponse
+from pimouse_ros.srv import TimedMotion                         # 追加
 
 class MotorTest(unittest.TestCase):
-    def setUp(self):                               #このメソッドを追加
-        rospy.wait_for_service('/motor_on',timeout=120)
+    def setUp(self):
+        rospy.wait_for_service('/motor_on')
         rospy.wait_for_service('/motor_off')
+        rospy.wait_for_service('/timed_motion')                 # 追加
         on = rospy.ServiceProxy('/motor_on', Trigger)
         ret = on()
 
@@ -50,7 +52,7 @@ class MotorTest(unittest.TestCase):
         self.file_check("rtmotor_raw_r0",0,"don't stop after 1[s]")
         self.file_check("rtmotor_raw_l0",0,"don't stop after 1[s]")
 
-    def test_on_off(self):                         #このメソッドも追加
+    def test_on_off(self):
         off = rospy.ServiceProxy('/motor_off', Trigger)
         ret = off()
         self.assertEqual(ret.success, True, "motor off does not succeeded")
@@ -66,6 +68,13 @@ class MotorTest(unittest.TestCase):
         with open("/dev/rtmotoren0","r") as f:
             data = f.readline()
             self.assertEqual(data,"1\n","wrong value in rtmotor0 at motor on")
+
+    def test_put_value_timed(self):                #このメソッドを追加
+        tm = rospy.ServiceProxy('/timed_motion', TimedMotion)
+        tm(-321,654,1500)
+        with open("/dev/rtmotor0","r") as f:
+            data = f.readline()
+            self.assertEqual(data,"-321 654 1500\n","value does not written to rtmotor0")
 
 if __name__ == '__main__':
     rospy.init_node('travis_test_motors')
